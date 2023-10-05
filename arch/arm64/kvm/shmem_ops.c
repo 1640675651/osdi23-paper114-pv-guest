2385db87a4f808101efe20b7bcc0cbc68db66144
simon-burke <simonburke1@mac.com>
2023-10-05 17:24:24 -0400

    attempt part 3

---
+++ arch/arm64/kvm/shmem_ops.c
@@ -0,0 +1,14 @@
+#include <kvm/pvops.h>
+
+
+unsigned long long get_shmem_size_op(void){
+    return kvm_pvops(HVC_GET_SHMEM_SIZE);
+} 
+EXPORT_SYMBOL(get_shmem_size_op);
+
+void* register_shmem_guest_op(unsigned long long base_addr) {
+    kvm_pvops(HVC_GUEST_SHMEM_REGISTER, base_addr);
+}
+EXPORT_SYMBOL(register_shmem_guest_op);
+
+
