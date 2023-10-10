431249d09ccb9263e6010a3aa60441f01d6101e6
1640675651 <1640675651@qq.com>
2023-10-10 23:48:56 +0000

    expose shmem functions

--- arch/arm64/kvm/shmem_guest.c
+++ arch/arm64/kvm/shmem_guest.c
@@ -1,7 +1,19 @@
 #include <kvm/shmem_guest.h>
-u64 get_shmem_size()
+struct guest_shmem_info shmem_info;
+
+u64 get_shmem_size(void)
 {
     return kvm_pvops(HVC_GET_SHMEM_SIZE);
+}
+
+u64 get_registered_size(void)
+{
+	return shmem_info.size;
+}
+
+u64 get_shmem_base(void)
+{
+	return shmem_info.base_phys;
 }
 
 // allocate memory, register as shared and return the virtual address
@@ -9,6 +21,8 @@ void* alloc_shmem_guest(u64 size)
 {
 	void* base_virt;
 	u64 base_phys; 
+	shmem_info.size = 0;
+	shmem_info.base_phys = 0;
 
 	printk(KERN_INFO "[SeKVM_Guest] Allocating %llu bytes Shared Memory\n", size);
 
@@ -23,5 +37,15 @@ void* alloc_shmem_guest(u64 size)
 	//printk(KERN_INFO "[SeKVM_Guest] Writing %d to the first byte of the shared memory\n", 12345);
     //*(int*) base_virt = 12345;
 
+	shmem_info.size = size;
+	shmem_info.base_phys = base_phys;
+
 	return base_virt;
 }
+
+//EXPORT_SYMBOL(get_shmem_size);
+EXPORT_SYMBOL(get_registered_size);
+EXPORT_SYMBOL(get_shmem_base);
+//EXPORT_SYMBOL(alloc_shmem_guest);
+//EXPORT_SYMBOL(__kvm_call_hyp);
+EXPORT_SYMBOL(shmem_info);
--- include/kvm/shmem_guest.h
+++ include/kvm/shmem_guest.h
@@ -5,7 +5,18 @@
 #include <linux/mman.h>
 #include <kvm/pvops.h>
 
+struct guest_shmem_info
+{
+    u64 size;
+    u64 base_phys;
+};
+
 u64 get_shmem_size(void);
 void* alloc_shmem_guest(u64 size);
+//u64 __kvm_call_hyp(void *hypfn, ...);
+
+extern struct guest_shmem_info shmem_info;
+extern u64 get_registered_size(void);
+extern u64 get_shmem_base(void);
 
 #endif
